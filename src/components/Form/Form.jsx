e56778a6f3033545378181429c55cc9de7b9e6fb
Thalisson <thalissonsousa33@gmail.com>
2024-10-08 22:24:49 -0300

    feat: add validate form and connect to api

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,32 @@
+/* eslint-disable react/prop-types */
 import './Form.css';
 import { Button, TextField } from "@mui/material";
+import axios from 'axios';
 
-export default function Form() {
+import { useForm } from "react-hook-form";
+
+export default function Form({ data }) {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm();
+
+    const onSubmit = (result) => axios.post('http://localhost:3330/add', result);
+
     return (
         <>
-            <form>    
-                <TextField id="outlined-basic" label="Mac" variant="outlined" />
-                <TextField id="outlined-basic" label="Regra ACL" variant="outlined" />
-                <Button variant="contained">Adicionar</Button>
+            <form>
+                <TextField id="outlined-basic" label="Mac" variant="outlined" {...register("mac", { required: true })} />
+                {errors.exampleRequired && <span>Campo obrigatório!</span>}
+                <div className='formInputs'>
+                    <TextField id="outlined-basic" label="Regra ACL" variant="outlined" defaultValue={data?.acl} {...register("acl", { required: true })} />
+                    {errors.exampleRequired && <span>Campo obrigatório!</span>}
+
+                    <TextField id="outlined-basic" label="Precedence" variant="outlined" defaultValue={data?.precedence} {...register("precedence", { required: true })} />
+                    {errors.exampleRequired && <span>Campo obrigatório!</span>}
+                </div>
+                <Button onClick={handleSubmit(onSubmit)} variant="contained">Adicionar</Button>
             </form>
         </>
     );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,21 +2,31 @@ import Form from '../../components/Form/Form';
 import ListScreen from '../../components/List/List';
 import './Home.css';
 
+import { useQuery } from '@tanstack/react-query';
+
 export default function Home() {
+
+    const { isPending, error, data } = useQuery({
+        queryKey: ['repoData'],
+        queryFn: () =>
+          fetch('http://localhost:3330/list').then((res) =>
+            res.json(),
+          ),
+      })
 
     return (
         <>
             <section>
                 <div className='Form'>
                     <h1>Liberar Mac</h1>
-                    <Form />
+                    <Form data={data} />
                 </div>
 
                 <div className='Divider'></div>
 
                 <div className='List'>
                     <h1>Listar Mac</h1>
-                    <ListScreen />
+                    <ListScreen isPending={isPending} error={error} data={data} />
                 </div>
             </section>
         </>
